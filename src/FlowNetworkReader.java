74e28477928b85a3ea51eef5fdc7345c26fc9f9b
Shribodhi Ranasinghe <shribodhi.ranasinghe@outlook.com>
2025-04-27 20:29:53 +0530

    Update the FlowNetworkReader.java

--- src/FlowNetworkReader.java
+++ src/FlowNetworkReader.java
@@ -1,7 +1,28 @@
-// Student ID: [ w2082808 / 20230401 ] - Name: [ Morathennage Shribodhi Indrakheela Ranasinghe ]
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-import java.util.*;
 public class FlowNetworkReader {
+
+    public static FlowNetworkSolver.Graph parseNetwork(String filename) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        int n = Integer.parseInt(br.readLine().trim());
+        FlowNetworkSolver.Graph graph = new FlowNetworkSolver.Graph(n);
+
+        String line;
+        while ((line = br.readLine()) != null){
+            String[] parts = line.trim().split("\\s+");
+            int from = Integer.parseInt(parts[0]);
+            int to = Integer.parseInt(parts[1]);
+            int capacity = Integer.parseInt(parts[2]);
+            graph.addEdge(from, to, capacity);
+        }
+        br.close();
+        return graph;
+    }
+
+
+
 
 
 
