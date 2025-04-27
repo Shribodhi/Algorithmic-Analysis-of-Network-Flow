f6d774690a2d2f7d447b86d87cc466cb6228bb12
Shribodhi Ranasinghe <shribodhi.ranasinghe@outlook.com>
2025-04-27 12:46:25 +0530

    Updating and creating the main class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Student ID: [ w2082808 / 20230401 ] - Name: [ Morathennage Shribodhi Indrakheela Ranasinghe ]
+
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        if(args.length != 1){
+            System.out.println("Usage: java Main <filename>");
+            return;
+        }
+        String filename = args[0];
+        try {
+            // Create a new instance of the FileReader class
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
         }
     }
 }
